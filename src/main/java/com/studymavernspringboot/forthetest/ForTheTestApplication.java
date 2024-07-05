576a66b403bbd0f5819665dc543425d216d8b7e9
whatnameshouldido <pobyjun@naver.com>
2024-07-05 17:05:23 +0900

    Initial commit

---
+++ src/main/java/com/studymavernspringboot/forthetest/ForTheTestApplication.java
@@ -0,0 +1,13 @@
+package com.studymavernspringboot.forthetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForTheTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForTheTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studymavernspringboot/forthetest/ForTheTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studymavernspringboot.forthetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForTheTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
